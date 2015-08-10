172e7c35c7e93fe67051dfee405f24ef527560d5
EmilyJohnson <3mle33@gmail.com>
2015-08-10 11:09:10 -0500

    put a fade on the nav

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
     var $window = $(window),
-        $navMenu = $('#nav-menu');
+        $navMenu = $('.home #nav-menu');
 
     console.log($navMenu);
     
     $window.scroll(function(){
         if ($window.scrollTop() > 200) {
-            $navMenu.addClass('expose');
+            $navMenu.addClass('expose').fadeIn();
             $navMenu.removeClass('go-ghost');
         } else {
-            $navMenu.addClass('go-ghost');
+            $navMenu.addClass('go-ghost').fadeOut();
             $navMenu.removeClass('expose');
         }
     });
