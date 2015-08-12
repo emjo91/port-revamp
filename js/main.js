ae6a1393bbbf92a5ac53d9c373d57cd721cffde2
EmilyJohnson <3mle33@gmail.com>
2015-08-12 11:17:18 -0500

    added in more accurate jQuery for the appearance of the nav, fine tuned text and padding. Added in links

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
 $(document).ready(function() {
     var $window = $(window),
-        $navMenu = $('.home #nav-menu');
-
-    console.log($navMenu);
-    
+        $navMenu = $('.home #nav-menu'),
+        $homeHeader = $('.home #header');
+        
+    // Fade in Navbar
     $window.scroll(function(){
-        if ($window.scrollTop() > 200) {
+        if ($window.scrollTop() > $homeHeader.height()) {
             $navMenu.addClass('expose').fadeIn();
             $navMenu.removeClass('go-ghost');
         } else {
             $navMenu.addClass('go-ghost').fadeOut();
             $navMenu.removeClass('expose');
         }
+    });
+    
+    // Spin the Cogs
+    var $cog = $('.about .content-area i');
+    
+    $cog.mouseenter(function(){
+        $(this).addClass('fa-spin');
+    });
+    
+    $cog.mouseleave(function(){
+        $(this).removeClass('fa-spin');
     });
     
 });
