bca62129bfa99b0142d17eea6d4d00af89dd3f2a
EmilyJohnson <3mle33@gmail.com>
2015-11-07 13:42:36 -0600

    added more content to the portfolio, added drop shadow to the greeting.

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer id="footer">
+    <div class="row">
+        <div class="column">
+            <nav id="footer-nav">
+                <ul class="social-media">
+                    <li><a target="_blank" href="https://www.facebook.com/emilyjohnsondesign"><i class="fa fa-facebook"></i></a></li>
+                    <li><a target="_blank" href="https://twitter.com/emjo91"><i class="fa fa-twitter"></i></a></li>
+                    <li><a target="_blank" href="https://github.com/emjo91"><i class="fa fa-github-alt"></i></a></li>
+                    <li><a target="_blank" href="https://www.linkedin.com/pub/emily-johnson/38/623/700
+"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</footer>
+</body>
---
+++ header.php
@@ -0,0 +1,79 @@
+<head>
+    <title>Portfolio</title>
+    
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="css/main.css" type="text/css" media="screen">
+    
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="css/font-awesome-4.3.0/css/font-awesome.css" type="text/css">
+    
+    <!-- Foundation -->
+    <link rel="stylesheet" href="css/foundation-5.5.2.custom/css/foundation.css" type="text/css" media="screen">
+    
+    <!-- Open Sans -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700,600,400italic,800' rel='stylesheet' type='text/css'>
+    
+    <!-- Over the Rainbow -->
+    <link href='http://fonts.googleapis.com/css?family=Over+the+Rainbow' rel='stylesheet' type='text/css'>
+    
+    <!-- jQuery -->    
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    
+    <script type="text/javascript" src="js/main.js"></script>
+    
+    <!-- DL Menu CSS -->
+    <link rel="stylesheet" type="text/css" href="css/dl-menu/css/component.css" />
+    
+    <!-- Modernizer -->
+    <script src="js/dl-js/modernizr.custom.js"></script>
+    
+    <!-- DL Menu jQuery -->
+    <script src="js/dl-js/jquery.dlmenu.js"></script>
+    
+    <!-- DL Menu Plugin Call -->
+    <script>
+    $(function() {
+      $( '#dl-menu' ).dlmenu();
+    });
+    </script>
+    
+    <!-- View Ports -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    
+</head>
+<body class="portfolio">
+<header id="header">
+    <nav id="nav-menu">
+        <div class="row">
+            <div class="logo columns small-6 medium-4">
+                <a href="index.html"><h1 class="header-nav">Emily Johnson</h1></a>
+            </div>
+            <div class="right-side-bar columns small-6 medium-8">
+                <!-- DL Menu -->
+                <div id="dl-menu" class="dl-menuwrapper">
+                    <button>Open <a href="http://www.jqueryscript.net/menu/">Menu</a></button>
+                    <ul class="dl-menu">
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="portfolio.html">Portfolio</a></li>
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="mailto:3mle33@gmail.com">Email</a></li>
+                    </ul>
+                </div>
+                <!-- End DL Menu -->
+                <div class="links">
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="portfolio.html">Portfolio</a></li>
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="mailto:3mle33@gmail.com">Email</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="secondary-header row">
+        <div class="columns small-12">
+            <h1 class="header">My Work</h1>
+        </div>
+    </div>
+</header>
